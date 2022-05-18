226a38ca3e8a0a4dbacd0bd839ad1fcf853ffc0d
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-05-18 14:12:23 -0400

    added code for tweet character counter and overhauled the CSS to make the website look more in line with what is recommended

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
-  // --- our code goes here ---
+$(document).ready(() => {
+
+  $("#tweet-text").on('input', function() {
+    
+    const maxTweet= 140;
+
+    let currentTweetLength= $(this).val().length;
+
+    let counter = $(this).parent().find("#counter")[0];
+
+    let remainingChars = maxTweet - currentTweetLength;
+
+    if (remainingChars < 0) {
+      $(counter).css('color', 'red');
+    } else {
+      $(counter).css('color', 'black');
+    }
+
+    $(counter).text(remainingChars);
+
+  });
+
 });
