c5193220f0bdf4d017389cf743989d7fcabdb2ce
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-05-28 17:37:10 -0400

    added some more comments to clarify what each style sheet does

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,24 +1,24 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
   //loops through tweets, takes return values and posts them based on oldest-newest tweets to the tweet container
   const renderTweets = (tweets) => {
     $('.tweet-display').empty();
     for (let tweet of tweets) {
-    $('.tweet-display').prepend(createTweetElement(tweet));
+      $('.tweet-display').prepend(createTweetElement(tweet));
     }
-  }
+  };
 
   //taken from compass, used to prevent code injection
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-  //creates the tweet element with the appropriate tags and returns it 
+  //creates the tweet element with the appropriate tags and returns it
   //to be used in the render function
   const createTweetElement = (tweet) => {
-    let newTweet= `
+    let newTweet = `
     <article class="tweet">
     <header>
       <div class="user-info">
@@ -43,27 +43,27 @@ $(document).ready(function () {
 
     return newTweet;
 
-  }
+  };
 
   //error message return for when there's too many chars
   const tooManyChars = () => {
-    let errMsg= `
+    let errMsg = `
     ⚠️ Too many characters! Must be 140 characters or less ⚠️`;
     $('.error-message').text(errMsg);
     
-  }
+  };
 
   //error message return for when there's no text in the box
   const emptyTextBox = () => {
-    let errMsg= ` 
+    let errMsg = ` 
     ⚠️ Your tweet is empty! Write out a tweet and try again :)`;
     $('.error-message').text(errMsg);
-  }
+  };
 
   //uses ajax get to call the rendertweets function
   const loadTweet = () => {
     $.get('/tweets', (tweets) => renderTweets(tweets));
-  }
+  };
 
   //submit tweets to page to be posted
   $("form").on('submit',function(event) {
@@ -75,14 +75,14 @@ $(document).ready(function () {
     let charCount = $('#tweet-text').val().length;
 
     if (!charCount) {
-      $('.error-message').slideDown('slow', emptyTextBox())
+      $('.error-message').slideDown('slow', emptyTextBox());
     } else if (charCount > 140) {
-      $('.error-message').slideDown('slow', tooManyChars())
+      $('.error-message').slideDown('slow', tooManyChars());
       
     } else {
-    $.post('/tweets',$(this).serialize())
-    .then(() => $('form').trigger('reset'))
-    .then(() => loadTweet());
+      $.post('/tweets',$(this).serialize())
+        .then(() => $('form').trigger('reset'))
+        .then(() => loadTweet());
     }
   });
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,9 @@ $(document).ready(() => {
 
   $("#tweet-text").on('input', function() {
     
-    const maxTweet= 140;
+    const maxTweet = 140;
 
-    let currentTweetLength= $(this).val().length;
+    let currentTweetLength = $(this).val().length;
 
     let counter = $(this).parent().find("#counter")[0];
 
