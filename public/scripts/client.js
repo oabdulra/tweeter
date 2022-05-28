063362b8f448154dd1d2c28594abf9c56050892d
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-05-28 00:05:22 -0400

    updated security for the website to prevent code injection

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -42,21 +42,27 @@ $(document).ready(function () {
     }
   }
 
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
   const createTweetElement = (tweet) => {
     let $newTweet= `
     <article class="tweet">
     <header>
       <div class="user-info">
         <div class="user-name-pic">
-          <img src="${tweet.user.avatars}">
-          <p>${tweet.user.name}</p> 
+          <img src="${escape(tweet.user.avatars)}">
+          <p>${escape(tweet.user.name)}</p> 
         </div>
-        <p class="user-handle">${tweet.user.handle}</p>
+        <p class="user-handle">${escape(tweet.user.handle)}</p>
       </div>
     </header>
-    <p class="tweet-message">${tweet.content.text}</p>
+    <p class="tweet-message">${escape(tweet.content.text)}</p>
     <footer class="time-like">
-      <p class="time-posted">${timeago.format(tweet.created_at)}</p> 
+      <p class="time-posted">${escape(timeago.format(tweet.created_at))}</p> 
       <div class="icons">
         <i class="fa-solid fa-flag"></i>
         <i class="fa-solid fa-retweet"></i>
